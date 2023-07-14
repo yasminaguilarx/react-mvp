24222b24eb671b24600ae6f2b5c4db277c8ab18b
yasminaguilarx <yaguilar2001@gmail.com>
2023-07-14 17:04:36 -0500

    changes

--- react-mvp-project/src/App.tsx
+++ react-mvp-project/src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import Posts from "./components/Posts";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [blogData, setBlogData] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch("./server/server.js/");
+      const data = await res.json();
+      setBlogData(data);
+    };
+    const [posts, setPosts] = useState([]);
+    getData();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>I am working</h1>
+      <Posts />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ react-mvp-project/src/components/Posts.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Posts() {
+  return <h1>Hello world</h1>;
+}
+
+export default Posts;
