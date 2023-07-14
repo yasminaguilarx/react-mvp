875beeefcdfbc8dd0fe26ef0b99ac0f632cecccc
yasminaguilarx <yaguilar2001@gmail.com>
2023-07-14 14:28:10 -0500

    changes

---
+++ react-mvp-project/server/seed.sql
@@ -0,0 +1,40 @@
+-- db name is react-mvp-app
+
+DROP TABLE IF EXISTS blog_posts CASCADE;
+DROP TABLE IF EXISTS comments CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+
+-- blog posts table
+CREATE TABLE blog_posts (
+post_id SERIAL PRIMARY KEY,
+post_title VARCHAR(50),
+blog_post TEXT
+);
+
+-- user table
+CREATE TABLE users (
+user_id SERIAL PRIMARY KEY,
+user_name VARCHAR(50),
+user_email TEXT
+);
+
+-- comments table
+CREATE TABLE comments (
+comment_id SERIAL PRIMARY KEY,
+comment_body VARCHAR(150)
+);
+
+--seed data into blog posts
+INSERT INTO blog_posts (post_id, post_title, blog_post)
+VALUES
+  (1, 'Introduction', 'Get to know me');
+
+--seed data into user table
+INSERT INTO users (user_id, user_name, user_email)
+VALUES
+  (1, 'example-name', 'example@gmail.com');
+
+--seed data into comments
+INSERT INTO comments (comment_id, comment_body)
+VALUES
+  (1, 'Simple introduction for me as a software engineer');
---
+++ react-mvp-project/server/server.js
@@ -0,0 +1,64 @@
+//import modules needed
+import pkg from "pg";
+const { Pool } = pkg;
+
+import express from "express";
+
+import dotenv from "dotenv";
+
+import cors from "cors";
+dotenv.config();
+
+const dbString = process.env.DATABASE_URL;
+
+const port = process.env.PORT;
+
+const pool = new Pool({
+  connectionString: dbString,
+});
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+
+//routes
+app.get("/blog_posts", async (req, res) => {
+  try {
+    const result = await pool.query(
+      "SELECT post_title, blog_post FROM blog_posts"
+    );
+
+    if (result.rowCount === 0) {
+      res.status(404).send("Not Found");
+    } else {
+      res.status(200).json(result.rows[0]);
+    }
+  } catch (err) {
+    console.error("Unable to grab posts for you!", err);
+    res.status(500).send("Internal Server Error: Blog Posts");
+  }
+});
+
+app.get("/blog_posts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await pool.query(
+      `SELECT * FROM blog_posts WHERE post_id = $1`,
+      [id]
+    );
+    if (result.rowCount === 0) {
+      res.status(404).send("Not Found");
+    }
+    res.status(200).send(result.rows[0]);
+  } catch (err) {
+    console.error("Unable to grab the post!", err);
+    res.status(500).send("Internal Server Error: Blog Post");
+  }
+});
+
+//listener
+app.listen(port, () => {
+  console.log(`I am listening on port: ${port}`);
+});
