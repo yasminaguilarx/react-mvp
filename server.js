6147f9fea2788dc973531c1c31d93e410b3e0894
yasminaguilarx <yaguilar2001@gmail.com>
2023-07-13 19:45:15 -0500

    changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import pg from "pg";
 
 const dbString = process.env.DATABASE_URL;
 const port = process.env.PORT;
-// const port = 3400;
+//const port = 3400;
 const pool = new pg.Pool({
   connectionString: dbString,
 });
@@ -20,8 +20,37 @@ app.use(cors({ origin: "*" }));
 
 //routes
 app.get("/blog_posts", async (req, res) => {
-  const result = await pool.query("SELECT * FROM blog_posts");
-  res.send(result);
+  try {
+    const result = await pool.query(
+      "SELECT (post_title, blog_post) FROM blog_posts"
+    );
+
+    if (result.rowCount === 0) {
+      res.status(404).send("Not Found");
+    } else {
+      res.status(200).send(result.rows[0]);
+    }
+  } catch (err) {
+    console.error("Unable to grab posts for you!", err);
+    res.status(500).send("Internal Server Error: Blog Posts");
+  }
+});
+
+app.get("/blog_posts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await pool.query(
+      `SELECT * FROM blog_posts WHERE post_id = $1`,
+      [id]
+    );
+    if (result.rowCount === 0) {
+      res.status(404).send("Not Found");
+    }
+    res.status(200).send(result.rows);
+  } catch (err) {
+    console.error("Unable to grab the post!", err);
+    res.status(500).send("Internal Server Error: Blog Post");
+  }
 });
 
 //listener
